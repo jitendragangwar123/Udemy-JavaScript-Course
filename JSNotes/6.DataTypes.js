8d287a9a7f23ae494dfad25c5e0e6b09fbb73ac4
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-03 12:39:40 +0530

    initial commit

--- JSNotes/6.DataTypes.js
+++ JSNotes/6.DataTypes.js
@@ -1,12 +1,21 @@
 const myAge=22; //Number DataType
 const amIAlive=true; //Boolean DataType
-const myName="Jeetu", // or 'Jeetu' String DataType
+const myName="Jeetu"; // or 'Jeetu' String DataType
 
 //In JS Array holds any kind of datatypes
-const arr=[22,true,"Jeetu"];  //Array DataType
+const arr1 = [22,true,"Jeetu"];  //Array DataType 
+console.log(arr1);
+
 
 //Put n at the end of Big Number during initialize the value
 const bigNumber=111111111111111111111111n;
 console.log(bigNumber);
 //Intialize null value 
 let someSpecialValue=null;
+
+
+//typeof Keyword
+const typeofBigNumber=(typeof bigNumber);
+console.log(typeofBigNumber); //bigint
+const typeofArr=(typeof arr1);
+console.log(typeofArr); //object
