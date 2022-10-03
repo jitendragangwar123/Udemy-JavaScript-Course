faa3ed331d7955f8912a518b6d6d28071c6cf20b
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-03 13:07:12 +0530

    initial commit

--- JSNotes/7. Math_Operator.js
+++ JSNotes/7. Math_Operator.js
@@ -17,3 +17,10 @@ console.log(score1); //18.5
 let powerOfNum=30;
 powerOfNum=powerOfNum**2; //powerOfNum^2;
 console.log(powerOfNum); //900
+
+// Find the average of students
+const friends=[20,30,40,50];
+const sum=friends[0]+friends[1]+friends[2]+friends[3];
+const totalFriends=4;
+const average=sum/totalFriends;
+console.log(average);
