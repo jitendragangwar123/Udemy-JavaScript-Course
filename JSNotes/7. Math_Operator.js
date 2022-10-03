84e6ccf9aa963ebfefa948f27b54a0f1c860c378
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-03 12:51:24 +0530

    initial commit

---
+++ JSNotes/7. Math_Operator.js
@@ -0,0 +1,19 @@
+const age=22;
+const updatedAge=age+1;
+console.log(updatedAge);
+
+let age1=11;
+age1=age1-1;
+console.log(age1);
+
+let score=20;
+score=score*2;
+console.log(score); //40
+
+let score1=37;
+score1=score1/2;
+console.log(score1); //18.5
+
+let powerOfNum=30;
+powerOfNum=powerOfNum**2; //powerOfNum^2;
+console.log(powerOfNum); //900
