79df00e66d5a8d978effd1398a58a65fc1582996
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-04 19:12:27 +0530

    initial commit

---
+++ JSNotes/10.Arrays.js
@@ -0,0 +1,26 @@
+//Arrays
+let myFriends=[];
+function addMyFriends(friend){
+    myFriends.push(friend);
+    console.log(myFriends);
+    }
+addMyFriends("Hari");
+addMyFriends("Ram");//[ 'Hari', 'Ram' ]
+
+//push add the value in the last
+//unshift add the value on top 
+let myFriends=[];
+function addMyFriends(friend){
+    myFriends.unshift(friend);
+    console.log(myFriends);
+    }
+addMyFriends("Hari");
+addMyFriends("Ram"); //[ 'Ram', 'Hari' ]
+
+//Remove the elements
+let removeFriends=myFriends.pop();
+console.log(myFriends); //[ 'Ram' ]
+
+//Update the elements
+myFriends[0]="Hari";
+console.log(myFriends); //[ 'Hari' ]
