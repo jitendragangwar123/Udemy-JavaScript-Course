20444ed51ca09ee17db0590b1bfabb777959800e
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-04 21:44:04 +0530

    initial commit

---
+++ JSNotes/11. Continue_and_Break.js
@@ -0,0 +1,19 @@
+//break statement
+for(let i=0;i<10;i++){
+    if(i===4){
+        break;
+    }
+    console.log(i) 
+}
+// 0 1 2 3
+
+
+//continue statement 
+for(let i=0;i<5;i++){
+    if(i===3){
+        continue;
+    }
+    console.log(i)
+}
+
+// 0 1 2 4
