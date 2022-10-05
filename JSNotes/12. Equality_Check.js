fc1104c9ec4525d28ef1e2fe51f418efbdfd671a
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-05 11:48:09 +0530

    initial commit

---
+++ JSNotes/12. Equality_Check.js
@@ -0,0 +1,6 @@
+let a = 1; // '=' used for assigning a value
+let b= 1==1; // true
+let c= 1=='1'; // true bcz it's not checking the type of variable 
+let d= 1===1; // true
+let e=1==='1'; //false bcz it's checking the type of variable 
+console.log(e);
