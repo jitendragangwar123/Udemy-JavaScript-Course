fcd669517a992703d6603a835df7c7d0c07741b6
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-05 12:05:07 +0530

    initial commit

---
+++ JSNotes/13. Objects.js
@@ -0,0 +1,37 @@
+//Objects
+const myObject={
+    "keyname1":"value1",
+    "keyname2":"value2"
+}
+console.log(myObject["keyname1"]); // value1
+
+//nested key
+const myObjects={
+    "keyname1":{
+        "keyname2.1":"value1"
+    },
+    "keyname2":"value2"
+}
+console.log(myObjects["keyname1"]["keyname2.1"]); //value1
+
+
+//By using dot operator
+const myObjects={
+    keyname1:{
+        "keyname2.1":"value1"
+    },
+    keyname2:"value2"
+}
+console.log(myObjects.keyname1["keyname2.1"]); //value1
+
+//Nested Object
+const myObjects={
+    keyname1:{
+        "keyname2.1":"value1",
+         obj2:{
+        "key":"v1"
+        }
+    },
+    keyname2:"value2"
+}
+console.log(myObjects.keyname1.obj2["key"]); //v1
