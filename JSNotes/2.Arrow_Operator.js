c3335678ebe11d206f7ab009eb2b6ef6bcac1dee
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-05 20:50:09 +0530

    initial commit

--- JSNotes/2.Arrow_Operator.js
+++ JSNotes/2.Arrow_Operator.js
@@ -1,47 +1,46 @@
-#Arrow Operator
-//const myFun=()=>doSomeThing();
-#pass the params
-//const myFun =(p1,p2)=>doSomeThing(p1,p2);
-#pass the single param
-//const myFun =p1=>doSomeThing(p1);
-#returning the value without using return keyword but it works only for single line statement
-// const myFun =()=>'test';
-// console.log(myFun());
-#returning the  object value  
-// const myFun =()=>({value:'test'});
-// console.log(myFun());
+//Arrow Operator
+const myFun=()=>doSomeThing();
+//pass the params
+const myFun =(p1,p2)=>doSomeThing(p1,p2);
+//pass the single param
+const myFun =p1=>doSomeThing(p1);
+//returning the value without using return keyword but it works only for single line statement
+const myFun =()=>'test';
+console.log(myFun());
+//returning the  object value  
+const myFun =()=>({value:'test'});
+console.log(myFun());
 
-
-#this keyword is not working with arrow operator because the value locked up into call stack so it's return undefined
-// const car = {
-//   model: 'Fiesta',
-//   manufacturer: 'Ford',
-//   fullName: () => {
-//     return `${this.manufacturer} ${this.model}`
-//   }
-// }
-// console.log(car.fullName());
+//this keyword is not working with arrow operator because the value locked up into call stack so it's return undefined
+const car = {
+   model: 'Fiesta',
+   manufacturer: 'Ford',
+   fullName: () => {
+     return `${this.manufacturer} ${this.model}`
+  }
+}
+ console.log(car.fullName());
 //output => undefined undefined
 
-#it will work fine with regular function()
-// const car = {
-//   model: 'Fiesta',
-//   manufacturer: 'Ford',
-//   fullName: function() {
-//     return `${this.manufacturer} ${this.model}`
-//   }
-// }
-// console.log(car.fullName());
+//it will work fine with regular function()
+const car = {
+  model: 'Fiesta',
+  manufacturer: 'Ford',
+  fullName: function() {
+    return `${this.manufacturer} ${this.model}`
+  }
+}
+console.log(car.fullName());
 //output => Ford Fiesta
 
-#this also not working with hadling the events.
-// const link = document.querySelector('#link')
-// link.addEventListener('click', () => {
-//   // this === window
-// })
+//this also not working with hadling the events.
+const link = document.querySelector('#link')
+link.addEventListener('click', () => {
+  // this === window
+})
 
-#working fine with regular function
-// const link = document.querySelector('#link')
-// link.addEventListener('click', function() {
-//   // this === link
-// })
+//working fine with regular function
+const link = document.querySelector('#link')
+link.addEventListener('click', function() {
+  // this === link
+})
