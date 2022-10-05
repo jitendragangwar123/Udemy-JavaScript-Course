566979b93fe3cbf1fba90a3215e29954c9a242ab
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-05 20:00:48 +0530

    initial commit

--- JSNotes/CounterApp/index.js
+++ JSNotes/CounterApp/index.js
@@ -1,14 +1,30 @@
 const incrementBtn=document.querySelector("#incrementcounter");
 const decrementBtn=document.querySelector("#decrementcounter");
 const counterEl=document.getElementById("counter");
+const listEl=document.getElementById("list-items");
 
 let counter=0;
+//Increment the counter
 function incrementCounter(){
     counter++;
     counterEl.innerText=counter;
+    //Create an element
+    let li=document.createElement("li");
+    //Set the attribute
+    li.setAttribute('data-counter',counter);
+    li.innerHTML="<b>Hello GFG</b>" +counter;
+    //Append the element
+    listEl.appendChild(li);
 }
-
+//Decrement the counter
 function decrementCounter(){
+
+    //Remove the element
+    const li=listEl.querySelector('[data-counter="'+counter+'"]');
+    const num=parseInt(li.getAttribute('data-counter'),10);
+    if(num%2===0){
+        li.remove();
+    }
     counter--;
     counterEl.innerText=counter;
 }
