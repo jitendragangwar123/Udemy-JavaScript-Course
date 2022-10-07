1fe2747ecdd330ed07ab846ea7974f2855ff823a
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-07 12:48:13 +0530

    initial commit

--- JSNotes/4.Template_Literals.js
+++ JSNotes/4.Template_Literals.js
@@ -1,3 +1,37 @@
-//
+//using `` for the string (ES6 version)
 const a_string = `something`;
 console.log(a_string);
+
+//old method
+const str='My Name is Jeetu.\
+\
+I am 30 years old.';
+console.log(str); //My Name is Jeetu.I am 30 years old.
+
+//new method 
+const str1=`My Name is Jeetu.
+I am 30 years old.`
+console.log(str1);
+//My Name is Jeetu.
+//I am 30 years old.
+
+
+//Old concept
+const myFriends={
+    Name:"Jeetu",
+    Age:22
+}
+
+const str1='My name is '+myFriends.Name+' and I am '+myFriends.Age+' years old.'
+console.log(str1); 
+//My name is Jeetu and I am 22 years old.
+
+//New Concept
+const myFriends1={
+    Name:"Jeetu",
+    Age:23
+}
+
+const str2=`My name is ${myFriends1.Name} and I am ${myFriends1.Age} years old.`
+console.log(str2); 
+
