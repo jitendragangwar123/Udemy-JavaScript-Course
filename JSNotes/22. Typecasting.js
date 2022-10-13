b9341e5f5142ea228dd7e085e5e17b9426cfbc3c
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-13 15:47:24 +0530

    initial commit

--- JSNotes/22. Typecasting.js
+++ JSNotes/22. Typecasting.js
@@ -4,9 +4,6 @@ val=1+"";
 console.log(val);// 1
 console.log(typeof val);//string 
 
-
-
-
 //string to int conversion 
 const string="15";
 const str1 =+string;
@@ -16,3 +13,13 @@ console.log(string); //15
 console.log(typeof str1); //number
 console.log(typeof str2); //number
 console.log(typeof str3); //number
+
+//Convert string to array
+const str="Jacob";
+const newArr1=str.split("");
+const newArr2=[...str];
+const newArr3=Array.from(str);
+
+console.log(newArr1); //[ 'J', 'a', 'c', 'o', 'b' ]
+console.log(newArr2); //[ 'J', 'a', 'c', 'o', 'b' ]
+console.log(newArr3); //[ 'J', 'a', 'c', 'o', 'b' ]
