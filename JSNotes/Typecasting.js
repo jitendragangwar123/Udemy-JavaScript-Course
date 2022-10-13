73d590236623e535a27878cc310d31bb3619d2bf
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-13 15:29:28 +0530

    initial commit

---
+++ JSNotes/Typecasting.js
@@ -0,0 +1,18 @@
+//int to string conversion 
+let val =1;
+val=1+"";
+console.log(val);// 1
+console.log(typeof val);//string 
+
+
+
+
+//string to int conversion 
+const string="15";
+const str1 =+string;
+const str2=Number(string);
+const str3=parseInt(string);
+console.log(string); //15
+console.log(typeof str1); //number
+console.log(typeof str2); //number
+console.log(typeof str3); //number
