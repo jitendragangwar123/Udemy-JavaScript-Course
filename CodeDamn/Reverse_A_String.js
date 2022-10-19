1b726876aef2916fb3d868576ff72aaa59afda84
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-19 14:31:47 +0530

    initial commit

--- CodeDamn/Reverse_A_String.js
+++ CodeDamn/Reverse_A_String.js
@@ -1,9 +1,25 @@
+//Reverse the String
 const str = "JavaScript is awesome"
-
 function reverseAString(str) {
     // write your solution here
     let res=str.split("").reverse().join("");
     return res;
 }
+console.log(`Reversed string is: ${reverseAString(str)}`);
 
-console.log(`Reversed string is: ${reverseAString(str)}`)
+//Reverse the String in Js by using for loop
+function reverseString(str){
+    let newString="";
+    for(let i=str.length-1;i>=0;i--){
+        newString+=str[i];
+    }
+    return newString;
+}
+console.log(`Reverseed String is : ${reverseString("Jacob")}`); // bocaJ
+
+//Reverse the String in Js by usnig reverse() method
+function reverseStr(str1){
+    return str1.split("").reverse().join("");
+}
+console.log(`Reverse String: ${reverseString("Jitendra")}`); // ardnetiJ
+
