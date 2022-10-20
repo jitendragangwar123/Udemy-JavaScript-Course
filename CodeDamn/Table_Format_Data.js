7edc463292571cc3e1db38e601baccf394d29eac
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-20 12:31:03 +0530

    initial commit

---
+++ CodeDamn/Table_Format_Data.js
@@ -0,0 +1,19 @@
+//Print the object data in Table format
+const obj={
+    Name:"Jay",
+    Age:25,
+    Gender:"Male"
+}
+console.table(obj);
+
+//Print the object data in Table format
+const Student={
+    Stu1:{FirstName:"Jitendra",SecondName:"Kumar",Age:24},
+    Stu2:{FirstName:"Amit",SecondName:"Kumar",Age:25},
+    Stu3:{FirstName:"Keshav",SecondName:"Kumar",Age:24},
+}
+console.table(Student);
+
+//Print the array in Table format
+let array=["Jeetu","Jay","Amit","Keshav"];
+console.table(array);
